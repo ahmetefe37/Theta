0056b0daf82ba334a932d7a927496e1c19fe0bb3
ahmetefe37 <ahmetefezorali@gmail.com>
2024-09-24 22:26:45 +0300

    Project Basics Setup 1.0

---
+++ src/test.cpp
@@ -0,0 +1,10 @@
+#include "test.h"	
+#include "stdio.h"
+
+namespace Theta {
+
+	void Print() 
+	{
+		printf("HELLO");
+	}
+}
---
+++ src/test.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace Theta {
+
+	__declspec(dllexport) void Print();
+
+}
